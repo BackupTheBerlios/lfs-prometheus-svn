1042e9444a11dc1f283a40dff390df7e1a7199c0
oliverbrakmann <oliverbrakmann@cfc14590-94df-0310-b30b-a18b4e926548>
2004-08-04 20:26:53 +0000

    Added GPL notice

--- branches/prometheus-0.1.x/pkgget.sh
+++ branches/prometheus-0.1.x/pkgget.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
 
+# Copyright (C) 2003-2004 Oliver Brakmann <obrakmann@gmx.net> &
+# Gareth Jones <gareth_jones@users.berlios.de>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+# Place, Suite 330, Boston, MA  02111-1307  USA
+
+
 # Usage:
 #
 # pkgget {[-h|-?|--help] | [{-f|--config} <config file>] [-l|--list] <package>
